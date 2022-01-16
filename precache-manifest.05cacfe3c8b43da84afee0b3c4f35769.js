434e34e64dfaefa4d8ef37be0655fb2f92917459
Pengdwende Abdoul GUira <guimani@yahoo.fr>
2022-01-16 17:48:33 -0500

    Updates

---
+++ precache-manifest.05cacfe3c8b43da84afee0b3c4f35769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615e38b1f4166b7ad97901abdb5c6473",
+    "url": "/Assignment_06_Project_2/index.html"
+  },
+  {
+    "revision": "1ca8e2073038e2b5cb07",
+    "url": "/Assignment_06_Project_2/static/css/main.7c2b9041.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/Assignment_06_Project_2/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "1ca8e2073038e2b5cb07",
+    "url": "/Assignment_06_Project_2/static/js/main.da539d1f.chunk.js"
+  },
+  {
+    "revision": "7435569f7037389dbb20",
+    "url": "/Assignment_06_Project_2/static/js/runtime~main.09fb7a6f.js"
+  }
+]);
